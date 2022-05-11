70f2eb7db087877bd843c12e6037ac9b8ae0c63e
bloodjuggles <bloodjuggles@gmail.com>
2022-05-12 00:13:14 +1000

    use updated reactflow example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import ReactFlow, { MiniMap, Controls } from 'react-flow-renderer';
+import React from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from 'react-flow-renderer';
 
-function Flow({ nodes, edges, onNodesChange, onEdgesChange, onConnect }) {
+import { nodes as initialNodes, edges as initialEdges } from './initial-elements';
+
+const onInit = (reactFlowInstance) => console.log('flow loaded:', reactFlowInstance);
+
+const OverviewFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = (params) => setEdges((eds) => addEdge(params, eds));
+
   return (
     <ReactFlow
       nodes={nodes}
@@ -11,11 +24,30 @@ function Flow({ nodes, edges, onNodesChange, onEdgesChange, onConnect }) {
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
+      onInit={onInit}
+      fitView
+      attributionPosition="top-right"
     >
-      <MiniMap />
+      <MiniMap
+        nodeStrokeColor={(n) => {
+          if (n.style?.background) return n.style.background;
+          if (n.type === 'input') return '#0041d0';
+          if (n.type === 'output') return '#ff0072';
+          if (n.type === 'default') return '#1a192b';
+
+          return '#eee';
+        }}
+        nodeColor={(n) => {
+          if (n.style?.background) return n.style.background;
+
+          return '#fff';
+        }}
+        nodeBorderRadius={2}
+      />
       <Controls />
+      <Background color="#aaa" gap={16} />
     </ReactFlow>
   );
-}
+};
 
-export default Flow;
+export default OverviewFlow;
